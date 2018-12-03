cc08b3460e5646df68343f2cdb2745c3e5413a08
Jason Bittel <jason.bittel@urbanairship.com>
2018-12-03 13:26:46 -0800

    Add tests

---
+++ mnemo/mnemo_test.go
@@ -0,0 +1,33 @@
+package mnemo_test
+
+import (
+	"testing"
+
+	"github.com/jbittel/mnemo/mnemo"
+)
+
+type tosTest struct {
+	input  int
+	output string
+}
+
+var tosTests = []tosTest{
+	{0, "ba"},
+	{1, "bi"},
+	{99, "zo"},
+	{100, "biba"},
+	{101, "bibi"},
+	{392406, "kogochi"},
+	{25437225, "haleshuha"},
+	{-1, "xabi"},
+	{-99, "xazo"},
+	{-100, "xabiba"},
+}
+
+func TestFmne_to_s(t *testing.T) {
+	for _, test := range tosTests {
+		if s := mnemo.Fmne_to_s(test.input); s != test.output {
+			t.Errorf("Input %d returned %s, expected %s", test.input, s, test.output)
+		}
+	}
+}
