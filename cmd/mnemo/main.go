37411a0a458372bb2a195e503e383f360564d6ba
Jason Bittel <jason.bittel@urbanairship.com>
2018-12-04 13:10:04 -0800

    Explicitly test if flags are set

--- cmd/mnemo/main.go
+++ cmd/mnemo/main.go
@@ -3,21 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
 
 	"github.com/jbittel/mnemo/mnemo"
 )
 
-func main() {
-	var encode int
-	var decode string
+type intFlag struct {
+	set   bool
+	value int
+}
 
-	flag.IntVar(&encode, "encode", 0, "Encode an integer to a string")
-	flag.StringVar(&decode, "decode", "", "Decode a string to an integer")
+func (flag *intFlag) Set(s string) error {
+	v, err := strconv.ParseInt(s, 0, strconv.IntSize)
+	flag.set = true
+	flag.value = int(v)
+	return err
+}
+
+func (flag *intFlag) String() string {
+	return strconv.Itoa(int(flag.value))
+}
+
+type stringFlag struct {
+	set   bool
+	value string
+}
+
+func (flag *stringFlag) Set(s string) error {
+	flag.set = true
+	flag.value = s
+	return nil
+}
+
+func (flag *stringFlag) String() string {
+	return flag.value
+}
+
+var encode intFlag
+var decode stringFlag
+
+func init() {
+	flag.Var(&encode, "encode", "Encode an integer to a string")
+	flag.Var(&decode, "decode", "Decode a string to an integer")
+}
+
+func main() {
 	flag.Parse()
 
-	if encode != 0 {
-		fmt.Println(mnemo.Encode(encode))
-	} else if decode != "" {
-		fmt.Println(mnemo.Decode(decode))
+	if encode.set {
+		fmt.Println(mnemo.Encode(encode.value))
+	} else if decode.set {
+		fmt.Println(mnemo.Decode(decode.value))
 	}
 }
