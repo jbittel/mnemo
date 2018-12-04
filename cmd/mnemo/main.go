cff52616b87b29fcb873e96e5e435aea2bb6064f
Jason Bittel <jason.bittel@urbanairship.com>
2018-12-04 08:47:55 -0800

    Implement rudimentary command line interface

--- cmd/mnemo/main.go
+++ cmd/mnemo/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	//"os"
 
 	"github.com/jbittel/mnemo/mnemo"
 )
 
 func main() {
-	arg := 25437225
-	fmt.Println(mnemo.Encode(arg))
+	var encode int
+	var decode string
+
+	flag.IntVar(&encode, "encode", 0, "Encode an integer to a string")
+	flag.StringVar(&decode, "decode", "", "Decode a string to an integer")
+	flag.Parse()
+
+	if encode != 0 {
+		fmt.Println(mnemo.Encode(encode))
+	} else if decode != "" {
+		fmt.Println(mnemo.Decode(decode))
+	}
 }
